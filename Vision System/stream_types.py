526870ddf7d474bfe00628409d654af0d9e1bc46
Bowen Thomas <thoby031@mymail.unisa.edu.au>
2022-09-03 12:03:54 +1000

    Add Edge Detector and Changed To Python Implementation

---
+++ Vision System/stream_types.py
@@ -0,0 +1,14 @@
+import cv2
+from enum import Enum, auto
+
+'''
+Enumerator Class For The Different Stream Types
+
+'''
+
+class Stream_Types(Enum):
+
+    VIDEO = auto()
+    CAMERA = auto()
+    IMAGE = auto()
+
